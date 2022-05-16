c7e815781197052a1db6c7d57193b2a3ab0a029d
buiba <babuimis.hvnh@gmail.com>
2022-05-16 20:39:09 +0700

    add demo signin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import GoogleLogin from "react-google-login";
+import "./App.css";
 
 function App() {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+  const handleLoginSuccess = async (googleData) => {
+    const res = await fetch("/api/google-login", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem("loginData", JSON.stringify(data));
+  };
+
+  const handleLoginFailure = (err) => {
+    console.log(`login failure with err: ${err}`);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        {loginData ? (
+          <div>
+            <h3>You logged in as {loginData.email}</h3>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Log in with Google"
+            onSuccess={handleLoginSuccess}
+            onFailure={handleLoginFailure}
+            cookiePolicy={"single_host_origin"}
+          ></GoogleLogin>
+        )}
+        {/* <LoginHooks /> */}
+        <div></div>
       </header>
     </div>
   );
